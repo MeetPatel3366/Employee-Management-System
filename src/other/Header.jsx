768db19b82ccee9d690e3731594f893538b1d4c9
Meet Patel <meet3366patel@gmail.com>
2025-01-12 09:13:53 +0530

    LogOut feature

--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const Header = ({data}) => {
-
+  
   // const [username,setUsername]=useState('')
 
   // if(!data)
@@ -12,10 +12,15 @@ const Header = ({data}) => {
   // {
   //   setUsername(data.firstName)
   // }
+
+  const logOutUser=()=>{
+      localStorage.setItem('loggedInUser','')
+      window.location.reload()
+  }
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>username 👋</span> </h1>
-        <button className='bg-red-600 text-white px-5 py-2 rounded-md text-lg font-medium'>Log Out</button>
+        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'> {data.firstName}👋</span> </h1>
+        <button onClick={logOutUser} className='bg-red-600 text-white px-5 py-2 rounded-md text-lg font-medium'>Log Out</button>
     </div>
   )
 }
